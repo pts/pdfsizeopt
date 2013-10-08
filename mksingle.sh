e14a65cd1b277b465689db1a8c293d45638b47aa
Peter Szabo <pts@fazekas.hu>
2013-10-08 14:29:52 +0200

    fixed \n generation in dash

--- mksingle.sh
+++ mksingle.sh
@@ -7,21 +7,23 @@
 set -ex
 rm -f pdfsizeopt.zip lib/__main__.py lib/mainrun.py
 
-echo 'import sys
+# In dash-0.5.5.1 as /bin/sh, \n is converted to a newline in single quotes, even
+# if we do it the tricy way, so we just use chr(10) instead.
+echo "import sys
 
 if not ((2, 4) <= sys.version_info[:2] < (3, 0)):
   sys.stderr.write(
-      "fatal: Python version 2.4, 2.5, 2.6 or 2.7 needed for: %s\n" % __file__)
+      'fatal: Python version 2.4, 2.5, 2.6 or 2.7 needed for: %s%c' % (__file__, 10))
   sys.exit(1)
 
 from pdfsizeopt import main
 main.zip_file = sys.path[0]
-sys.exit(main.main(sys.argv))' >>lib/mainrun.py
+sys.exit(main.main(sys.argv))" >lib/mainrun.py
 # We need a file other than __main__.py, because 'import __main__' below
 # doesn't load __main__.py.
 # TODO(pts): Can we use `-m mainrun'? Does it work in Python 2.0, 2.1, 2.2
 # and 2.3? (So that we'd reach the proper error message.)
-echo 'import mainrun' >>lib/__main__.py
+echo 'import mainrun' >lib/__main__.py
 
 (cd lib && zip -9r ../pdfsizeopt.zip \
     __main__.py mainrun.py \
@@ -36,6 +38,9 @@ test -f pdfsizeopt.zip
 #
 # You need Python 2.4, 2.5, 2.6 or 2.7 to run this script. The shell script
 # below tries to find such an interpreter and then runs it.
+#
+# If you have Python 2.6 or Python 2.7, you can also run it directly with
+# Python, otherwise you have to run it as a shell script.
 #
 
 type python2.7 >/dev/null 2>&1 && exec python2.7 -- "$0" ${1+"$@"}
